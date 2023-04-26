cf119c62b32aab1dd305492378602f185e9cf9e1
Yanlin0207 <craniumwhoim@gmail.com>
2023-04-26 17:25:16 +0800

    modules建立完成

---
+++ src/app/porfile/porfile.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PorfileRoutingModule } from './porfile-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PorfileRoutingModule
+  ]
+})
+export class PorfileModule { }
