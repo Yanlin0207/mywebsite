56540701e8c2fe7f082c509eb91a298072fafcca
Yanlin0207 <craniumwhoim@gmail.com>
2023-05-02 15:57:21 +0800

    新增component

---
+++ src/app/resume/resume-menu/resume-menu.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ResumeMenuComponent } from './resume-menu.component';
+
+describe('ResumeMenuComponent', () => {
+  let component: ResumeMenuComponent;
+  let fixture: ComponentFixture<ResumeMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ResumeMenuComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ResumeMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
