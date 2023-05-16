356fe69ef477169c53049de832c5257a9c29209b
Yanlin0207 <craniumwhoim@gmail.com>
2023-05-16 21:29:03 +0800

    看板剩下下半部

--- src/app/portfolio/portfolio-detail/portfolio-detail.component.ts
+++ src/app/portfolio/portfolio-detail/portfolio-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-portfolio-detail',
@@ -9,14 +10,15 @@ import { ActivatedRoute } from '@angular/router';
 export class PortfolioDetailComponent implements OnInit {
   id = 0
   title = ''
-  description=''
+  description = ''
+  intro_video = false
 
   data = [
     {
       id: '0',
       title: '設備資訊管理平台',
       src: '../../../assets/eim/eim-banner.png',
-      description:'整體系統 : 參與部分功能發想規劃。\nUI/UX : 參與架構規劃。\n前端 : 前端架構規劃設計、API串接資料設計、實作頁面。\n前端實作項目 : 機台、表單設計、派工、戰情看板-機台詳細資訊、戰情看板-全球概況、報表-DI曝光頭模組狀態、報表-機台總覽、系統-Tag。\n後端實作項目 : 派工-工作排程(mobile)、報表-DI曝光頭模組狀態。',
+      description: '◎ 整體系統 : 參與部分功能發想規劃。\n◎ UI/UX : 參與架構規劃。\n◎ 前端 : 前端架構規劃設計、API串接資料設計、實作頁面。\n◎ 前端實作項目 : 機台、表單設計、派工、戰情看板-機台詳細資訊、戰情看板-全球概況、報表-DI曝光頭模組狀態、報表-機台總覽、系統-Tag。\n◎ API實作項目 : 派工-工作排程(mobile)、報表-DI曝光頭模組狀態。',
       intro: [
         {
           src: '../../../assets/eim/1.PNG',
@@ -43,6 +45,7 @@ export class PortfolioDetailComponent implements OnInit {
           text: 'RWD行動裝置版本。'
         }
       ],
+      intro_video: false,
       tools: [
         {
           name: 'Angular',
@@ -101,11 +104,32 @@ export class PortfolioDetailComponent implements OnInit {
           src: 'git.webp'
         },
       ],
+    },
+    {
+      id: '1',
+      title: '戰情看板',
+      src: '../../../assets/eim/eim-banner.png',
+      description: '',
+      intro: [
+        {
+          src: '../../../assets/chitec/1.PNG',
+          text: '所有設備狀況一目瞭然，發生警報的設備隨時能在上方看見。'
+        },
+        {
+          src: '../../../assets/chitec/2.PNG',
+          text: '各設備詳細運作狀況，發生警報的設備會顯示在最上方和側邊選單。'
+        },
+        {
+          src: '../../../assets/chitec/3.PNG',
+          text: '設備數據折線圖。'
+        },
+      ],
+      intro_video: true,
     }
   ]
 
   constructor(
-    private _aRoute: ActivatedRoute,
+    private _aRoute: ActivatedRoute, private _sanitizer: DomSanitizer
   ) {
   }
 
@@ -115,6 +139,7 @@ export class PortfolioDetailComponent implements OnInit {
     });
 
     this.title = this.data[this.id].title
-    this.description=this.data[this.id].description
+    this.intro_video = this.data[this.id].intro_video
+    this.description = this.data[this.id].description
   }
 }
