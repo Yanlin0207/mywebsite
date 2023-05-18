28cec58542badfed82b564bef5f7cc1302fcfb26
Yanlin0207 <craniumwhoim@gmail.com>
2023-05-18 13:55:06 +0800

    全部圖檔改成絕對路徑

--- src/app/portfolio/portfolio-detail/portfolio-detail.component.ts
+++ src/app/portfolio/portfolio-detail/portfolio-detail.component.ts
@@ -21,27 +21,27 @@ export class PortfolioDetailComponent implements OnInit {
       description: '◎ 整體系統 : 參與部分功能發想規劃。\n◎ UI/UX : 參與架構規劃。\n◎ 前端 : 前端架構規劃設計、API串接資料設計、實作頁面。\n◎ 前端實作項目 : 機台、表單設計、派工、戰情看板-機台詳細資訊、戰情看板-全球概況、報表-DI曝光頭模組狀態、報表-機台總覽、系統-Tag。\n◎ API實作項目 : 派工-工作排程(mobile)、報表-DI曝光頭模組狀態。\n◎ 專案位置 : 現場DEMO。 ',
       intro: [
         {
-          src: '../../../assets/eim/1.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/eim/1.PNG',
           text: '自由的表單設計，客製自己想要的欄位樣式。'
         },
         {
-          src: '../../../assets/eim/2.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/eim/2.PNG',
           text: '各模組狀態清楚明瞭。'
         },
         {
-          src: '../../../assets/eim/3.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/eim/3.PNG',
           text: '詳細列出機台欄位資訊。'
         },
         {
-          src: '../../../assets/eim/4.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/eim/4.PNG',
           text: '視覺化圖表及原始資料查看。'
         },
         {
-          src: '../../../assets/eim/5.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/eim/5.PNG',
           text: '視覺化概況。'
         },
         {
-          src: '../../../assets/eim/6.png',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/eim/6.png',
           text: 'RWD行動裝置版本。'
         }
       ],
@@ -112,15 +112,15 @@ export class PortfolioDetailComponent implements OnInit {
       description: '◎ UI/UX : 參與部分設計。\n◎ 前端 : API串接資料設計、實作頁面。\n',
       intro: [
         {
-          src: '../../../assets/chitec/1.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/chitec/1.PNG',
           text: '所有設備狀況一目瞭然，發生警報的設備隨時能在上方看見。'
         },
         {
-          src: '../../../assets/chitec/2.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/chitec/2.PNG',
           text: '各設備詳細運作狀況，發生警報的設備會顯示在最上方和側邊選單。'
         },
         {
-          src: '../../../assets/chitec/3.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/chitec/3.PNG',
           text: '設備數據折線圖。'
         },
       ],
@@ -167,15 +167,15 @@ export class PortfolioDetailComponent implements OnInit {
       description: '◎ 整體系統 : 參與部分功能發想規劃。\n◎ UI/UX : 設計執行。\n◎ 前端 : API串接資料設計、實作頁面。\n',
       intro: [
         {
-          src: '../../../assets/ehm/1.png',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/ehm/1.png',
           text: '單一設備監控狀況，提供圖表切換資料切換。'
         },
         {
-          src: '../../../assets/ehm/2.PNG',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/ehm/2.PNG',
           text: '彈性設定圖表臨界值。'
         },
         {
-          src: '../../../assets/ehm/3.png',
+          src: 'https://yanlin0207.github.io/mywebsite/assets/ehm/3.png',
           text: '單一設備的即時影像及監控狀況。'
         },
       ],
