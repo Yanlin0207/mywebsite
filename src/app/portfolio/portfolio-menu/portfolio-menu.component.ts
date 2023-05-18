d151aa9faee6abfb61c59459e3db06f756f67133
Yanlin0207 <craniumwhoim@gmail.com>
2023-05-18 13:31:27 +0800

    試試改成絕對路徑會不會讓github page讀得到圖檔

--- src/app/portfolio/portfolio-menu/portfolio-menu.component.ts
+++ src/app/portfolio/portfolio-menu/portfolio-menu.component.ts
@@ -10,17 +10,17 @@ export class PortfolioMenuComponent {
     {
       id: '0',
       title: '設備資訊管理平台',
-      src:'../../../assets/eim/eim-cover.png'
+      src:'https://yanlin0207.github.io/mywebsite/assets/eim/eim-cover.png'
     },
     {
       id: '1',
       title: '戰情看板',
-      src:'../../../assets/chitec/chitec-cover.png'
+      src:'https://yanlin0207.github.io/mywebsite/assets/chitec/chitec-cover.png'
     },
     {
       id: '2',
       title: '設備建康管理',
-      src:'../../../assets/ehm/ehm-cover.png'
+      src:'https://yanlin0207.github.io/mywebsite/assets/ehm/ehm-cover.png'
     }
   ]
 
